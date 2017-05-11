1b30b42e6f454a894c5ae703c15dfbc7ca6c0ed2
JFYVART <jean.francois.yvart@gmail.com>
2017-05-11 10:25:29 +0200

    Géolocalisation et premier marqueur sur la carte

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -34,9 +34,46 @@ function init() {
 	}
 	/*creation de la map*/
 	map = new google.maps.Map(document.getElementById("divMap"), myOptions);
+	
+	var latLng = new google.maps.LatLng(43.6044600, 1.4439700);
+	var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+	
 	/*connexion de la map + le panneau de l'itinéraire*/
 	directionsDisplay.setMap(map);
 	directionsDisplay.setPanel(document.getElementById("divRoute"));
 	/*intialise le geocoder pour localiser les adresses */
 	geocoder = new google.maps.Geocoder();
-}
+	
+	
+	// Géolocalisation
+	var infoWindow = new google.maps.InfoWindow({map: map});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+  }
